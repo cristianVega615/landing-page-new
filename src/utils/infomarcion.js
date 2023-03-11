07393fc4bbd384a8d198912d7eba42d01c6b0cba
Cristian Vega <cristiancode615@gmail.com>
2023-03-11 14:17:04 -0500

    Me falta implementar el dark mode, además que pueda poner una section para iniciar sesión.

--- src/utils/infomarcion.js
+++ src/utils/infomarcion.js
@@ -1,4 +1,4 @@
-export { hostingCompartido }
+export { hostingCompartido, servidoresPrivados , servidoresDedicados, servidoresCloud }
 
 const hostingCompartido = {
     title: "Hosting Compartido",
@@ -11,5 +11,45 @@ const hostingCompartido = {
     desventajas: {
         title: "Desventajas",
         lista: ["Ya que compartimos recursos con otros sitios web, puede haber limitaciones en la cantidad de recursos que puedes utilizar, como el espacio de disco, la memoria RAM, etc", "El rendimiento del servidor puede verse afectado por otros sitios web que comparten los mismos recursos", "Limitaciones en la personalización debido a que el servidor está configurado para alojar varios sitios web, es posible que no puedas personalizar tanto como un servidor dedicado", "La falta de control ya que no tienes control del software y las configuraciones del servidor."]
+    }
+}
+const servidoresPrivados = {
+    title: "Servidores Privados",
+    id: "server-private",
+    info: "Los servidores son servidores de computadora que están reservados para el uso exclusico de una organización o individuo en particular. A diferencia de los servidores públicos, que son diferencia de propiedad de empresas de alojamiento web y ofrecen sus servicios de alojamientos a múltiples usuarios, los servidores privados se utilizan para alojar sitios web, aplicaciones y servicios de una sola entidad",
+    ventajas: {
+        title: "Ventajas",
+        lista: ["Al utilizar un servidor privado, se tiene un control completo sobre la configuración, seguridad y personalización del servidor. Esto significa que se puede adaptar el servidor a las necesidades especificas de la organización o proyecto", "Al ser un servidor privado, se reduce el riesgo de ataques malintencionados, ya que solo la organización o individuo que lo utiliza tiene acceso a él.", "Son escalables, lo que significa que se pueden agregar recursos aidicionales según se necesite.", "En caso de problemas técnicos, la empresa de alojamiento web que proporciona el servidor privado generalmente oferce soporte técnico para ayudar a solucionar los problemas"]
+    },
+    desventajas: {
+        title:"Desventaja",
+        lista:["Suelen ser más caros que los servidores compartidos, ya que se pega por recursos exclusivos", "Se asume la responsabilidad de administrarlo y mantenerlo, lo que puede requirir habilidades técnicas y tiempo para hacerlo correctamente", "En caso de problema técnicos con el servidor, puede ser necesario buscar y solucionar el problema por cuenta propia o contratar un técnico para que lo haga. Esto puede ser costoso y consumir tiempo", "Aunque los servidores privados son escalables, el proceso de agregar más recursos puede ser complicado y puede requerir un reconfiguración significativa del servidor"]
+    }
+
+}
+const servidoresDedicados = {
+    title: "Servidores Dedicados",
+    id: "server-dedicated", 
+    info: "Los servidores dedicados son computadoras físicas que están configuradas y utilizadas exclusivamente para alojar contenido web o de aplicaciones para una sola organización o cliente. En otras palabras, un servidor dedicado es un servidor completo que se reserva para un solo usuario, en lugar de ser compartido por muchos usuarios, como es el caso con la mayoría de los servicios de alojamiento web.",
+    ventajas: {
+        title: "Ventajas",
+        lista: ["Tiene un rendimiento mejorado, estos ofrecen un alto nivel de rendimiento y velocidad, ya que el cliente tiene acceso exclusivo a todo los recursos del hardware.", "El cliente tiene un control total sobre el servidor, lo que le permite personalizar el entorno del servidor según sus necesiades específicas.", "Un servidor dedicado ofrece un nivel de seguridad más alto que otros tips de alojamiento web.", "Los servidores dedicados son altamente flexibles y escalables. El cliente puede actualizar o agregar hardware según sea necesario para satisfacer las necesidades de su sitio web o aplicación."]
+    },
+    desventajas: {
+        title:"Desventajas",
+        lista: ["El costo de adquirir y mantener un servidor dedicado es mucho más alto que el de otros tiposde alojamiento web. Además del costo inicial de adquisición del hardware.", "Su complejidad en configurar y administrar un servidor dedicado puede ser complejo y requerir un nivel de conocimiento técnico avanzado.", "A diferencia de otros tipos de alojamiento web en los que el proveedor es responsable del mantenimiento y reparación del servidor, en un servidor dedicado el cliente es responsable de cualquier falla o problema de hardware", "Los servidores dedicados son menos escalables debido a que si hay un aumento en el tráfico, entonces el ciente debería actualizar el hardware del servidor"]
+    }
+}
+const servidoresCloud = {
+    title: "Servidores Cloud",
+    id: "server-cloud", 
+    info: "Son una forma de alojamiento web que utiliza una red de servidores remotos para almacenar, procesar y gestionar datos, aplicaciones y servicios en línea. En lugar de depender de un solo servidor físico, la infraestructura de la nube está compuesta por un conjunto de servidores interconectados y distribuidos geográficamente, lo que permite a los usuarios acceder a sus recursos de manera flexible y escalable según sus necesidades.",
+    ventajas: {
+        title:"Ventajas",
+        lista: ["La infraestructura de la nube está diseñada para ofrecer alta disponibilidad y redundancia. Los datos y aplicaciones se almacenan en varios servidores y centros de datos, lo que minimiza el riesgo de interrupciones o fallas.", "Los servidores cloud ofrecen un modelo de pago flexible y escalable, lo que permite a los usuarios ajustar sus costos en función de sus necesidades y presupuestos.", "Los proveedores de servicios de la nube se encargan de la gestión de la infraestructura, lo que libera a los usuarios de la carga de mantener y actualizar los servidores.", "Los proveedores de servicios de la nube ofrecen medidas de seguridad avanzadas, como firewalls, monitoreo constante, cifrado y copias de seguridad"]
+    },
+    desventajas: {
+        title: "Desventajas",
+        lista: [" Los servidores cloud dependen de una conexión a Internet estable y rápida para acceder a los recursos y servicios en línea. Si la conexión falla o es lenta, puede afectar la disponibilidad y el rendimiento de los servicios.", "Aunque los proveedores de servicios de la nube ofrecen medidas de seguridad avanzadas, la seguridad de los datos y aplicaciones sigue siendo una preocupación para muchos usuarios.", "Aunque los costos iniciales pueden ser más bajos que con una solución de alojamiento en sitio, los costos a largo plazo pueden ser más altos si se utiliza una gran cantidad de recursos o se requieren características específicas.", " Los usuarios no tienen control total sobre la infraestructura de la nube, lo que puede limitar la personalización y la capacidad de ajustar ciertos aspectos técnicos de la infraestructura."]
     }
 }
