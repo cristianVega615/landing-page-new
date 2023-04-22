c2f5ee653985dabbc17976d9386edfde4ecdbdaa
Cristian Vega <cristiancode615@gmail.com>
2023-04-22 17:34:26 -0500

    Estuve haciendo el login y el signIn, las vistas ya estan casi completas.

--- src/scripts/loader.ts
+++ src/scripts/loader.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 
 
 const btnLogin = document.querySelector(".btn-login") as HTMLButtonElement;
+const btnSign = document.querySelector(".btn-sign") as HTMLButtonElement;
 const formLogin = document?.querySelector(".form-login");
 window.addEventListener("load", (event) => {
   const content = document.querySelector(".container_loader") as HTMLDivElement;
@@ -35,26 +36,39 @@ btnLogin?.addEventListener("click", () => {
     })
     .then((response) => {
       window.location.href = "http://localhost:3000/"
-
-      // document.cookie = response.data?.cookie
-      console.log(document.cookie)
-      console.log(response);
     });
 
 
-  // fetch("http://localhost:3002/api/login", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(informacion),
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //   }).catch(
-  //     err => console.log(err)
-  //   );
 });
 
+btnSign?.addEventListener("click", () => {
+  const email = document.querySelector(".email") as HTMLInputElement;
+  const password = document.querySelector(".password") as HTMLInputElement;
+  const nombre = document.querySelector(".nombre") as HTMLInputElement;
+  const apellido = document.querySelector(".apellido") as HTMLInputElement;
+  const numero = document.querySelector(".numero") as HTMLInputElement;
+  const informacion = {
+    email: email.value,
+    password: password.value,
+    nombre: nombre.value,
+    apellido: apellido.value,
+    numero: numero.value
+  };
+
+  axios
+    .post("http://localhost:3002/api/signIn", informacion, {
+      headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":"Content-Type",
+        "Access-Control-Max-Age": "86400"
+      },
+      withCredentials: true,
+    })
+    .then((response) => {
+      window.location.href = "http://localhost:3000/"
+      console.log(response)
+    });
+})
 export {};
